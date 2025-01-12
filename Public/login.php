dcc28df0c0c3525486572a83a011d6a1b5270bf4
SubCannon <subcannon_bf44@hotmail.com>
2025-01-13 00:49:12 +0300

    Giris yapan kullanıcı tekrar logine giremicek sekilde degistirildi.

--- Public/login.php
+++ Public/login.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+if (isset($_SESSION['user_id'])) {
+    header('Location: index.php'); // Kullanıcı giriş yapmışsa yönlendir
+    exit();
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
